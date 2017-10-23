dd625ef410add7199b1e688afb9bd7510b4952d2
Richman Clifford <larriche99@gmail.com>
2017-10-23 22:23:24 +0000

    Started working on linked list implementation

---
+++ Linked Lists/LinkedList.java
@@ -0,0 +1,95 @@
+public class LinkedList<Item>
+{
+	private class Node
+	{
+		// Data item that the node is storing
+		public Item item;
+
+        // The next node this node points to
+        // or null if terminal node
+		public Node next;
+
+		public Node(Item item)
+		{
+			this.item = item;
+			this.next = null;
+		}
+	}
+
+    // The head node
+	private Node head;
+
+    // The tail node
+	private Node tail;
+
+	public LinkedList()
+	{
+		this.head = null;
+		this.tail = null;
+	}
+
+    /**
+     * Create a new node out of the given data item
+     * and add it as first node of the linked list
+     * 
+     * @param item The data item
+     */
+	public void insertAtBegining(Item item)
+	{
+		// Store the current head
+		Node old_head = this.head;
+
+		// Create a new node and let this be the head 
+		// node
+		this.head = new Node(item);
+		this.head.next = old_head;
+	}
+
+    /**
+     * Delete the first node of a linked list
+     * 
+     */
+	public void deleteFromBegining()
+	{
+
+	}
+
+    /**
+     * Add an item to the end of a linked list
+     * 
+     * @param item The data item
+     */
+	public void insertAtEnd(Item item)
+	{
+	    Node old_last = this.tail;	
+	    Node last = new Node(item);
+
+	    if (this.head == null) {
+	    	this.head = last;
+	    	this.tail = this.head;
+	    } else {
+	    	this.old_last.next = last;
+	    }
+	}
+
+    /**
+     * Add a new node with the given data after the node
+     * storing the given key 
+     * 
+     * @param key  Data whose node we want to insert after
+     * @param item [description]
+     */
+	public void insertAfter(Item key, Item item)
+	{
+		Node curr = this.head;
+		Node new_node = new Node(item);
+
+		while (curr != null) {
+			if (curr.item == key) {
+				new_node.next = curr.next;
+				curr.next = new_node;
+				break;
+			}
+		}
+	}
+}
